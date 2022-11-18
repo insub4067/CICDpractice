1ba03fab25fe5996e26aa09483eda2711d6fe2fd
insub4067 <vb15@kakao.com>
2022-11-18 12:06:48 +0900

    init

---
+++ CICDpractice/CICDpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDpracticeApp.swift
+//  CICDpractice
+//
+//  Created by Kim Insub on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct CICDpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
